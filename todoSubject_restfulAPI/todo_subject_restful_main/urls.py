90a8ac200066b455aa086a34256cf164adfe5d25
ksm0207 <ksm03071@naver.com>
2020-09-16 08:08:50 +0900

    restapi 환경설정 세팅완료

---
+++ todoSubject_restfulAPI/todo_subject_restful_main/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from django.urls import path
+
+app_name = "todo_subject_restful_main"
+
+router = routers.DefaultRouter()
+router.register(r"todo_board", views.Todo_subject_restful_main)
+urlpatterns = [
+    path("api-auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todoSubject_restfulAPI/todo_subject_restful_main/views.py
+++ todoSubject_restfulAPI/todo_subject_restful_main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import TodoList
+from .serializers import TodoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class Todo_subject_restful_main(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
