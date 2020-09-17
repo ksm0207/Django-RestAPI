ff2616b51a6d6bc9cb501e45bc5fbbb26afeeee8
ksm0207 <ksm03071@naver.com>
2020-09-18 05:23:52 +0900

    RestAPI - Update / Delete VIew 생성

--- todoRestAPI/todo_subject_restful_main/urls.py
+++ todoRestAPI/todo_subject_restful_main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 # from rest_framework import routers
 
 app_name = "todo_subject_restful_main"
@@ -14,6 +13,9 @@ urlpatterns = [
     path("", views.TodoMainView.as_view(), name="todo_list"),
     # url(r'^todo_list/(?P<no>\d+)/$', views.TodoRestDetailView.as_view(), name='todo_detail'),
     path("todo_list/<int:no>/", views.TodoRestDetailView.as_view(), name='todo_detail'),
+    path("todo_list/<int:no>/update", views.TodoRestUpdateView.as_view(), name='todo_update'),
+    path("todo_list/<int:no>/delete", views.TodoDeleteView.as_view(), name='todo_delete'),
+
 
 ]
 
--- todoRestAPI/todo_subject_restful_main/views.py
+++ todoRestAPI/todo_subject_restful_main/views.py
@@ -3,16 +3,36 @@ from .serializers import TodoSerializer, TodoDetailSerializer
 from .models import TodoList
 
 # from rest_framework import viewsets
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
 
+# ListView
 class TodoMainView(ListAPIView):
     queryset = TodoList.objects.all()
     serializer_class = TodoSerializer
 
 
+# DetailView
 class TodoRestDetailView(RetrieveAPIView):
     lookup_field = "no"
     queryset = TodoList.objects.all()
     serializer_class = TodoDetailSerializer
 
+
+# UpdateView
+class TodoRestUpdateView(UpdateAPIView):
+    lookup_field = "no"
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
+
+
+# DeleteView
+class TodoDeleteView(DestroyAPIView):
+    lookup_field = "no"
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
